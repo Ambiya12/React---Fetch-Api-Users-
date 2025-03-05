6f7bd259a7c94d590f56783f706b521245c9dc7f
Ambiya12 <ambiyadms@gmail.com>
2025-03-05 11:22:56 +0100

    add App component and styling for user display

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import {useEffect, useState} from 'react';
+import './App.css';
+import axios from 'axios';
+
+const App = () => {
+
+  const myName = 'Galyst';
+  const [users, setUsers] = useState(null);
+
+  const fetchAPI = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/api/users');
+      setUsers(response.data);
+    }
+    catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  return (
+    <>
+      <h1 className='myH1'>Welcome</h1>
+      <p>Hello my name is {myName}</p>
+   
+      {users && users.map(user => {
+        return (
+          <div key={user.id} style={{border: '1px solid black', margin: '10px', padding: '10px'}}>
+            <p>First Name : {user.firstName}</p>
+            <p>Last Name : {user.lastName}</p>
+            <p>Telephone : {user.telephone}</p>
+            <p>Address : {user.address}</p>
+            <p>Hobbies : {user.hobbies}</p>
+          </div>
+        )
+      })}
+    </>      
+  );
+}
+
+export default App;
